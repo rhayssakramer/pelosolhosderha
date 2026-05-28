f6102b9b9ed94aaa4ace117d68013dbf9a44c2ae
rhayssakramer <rhayssakramer@hotmail.com>
2026-05-28 16:33:31 -0300

    feat(frontend): implementa autenticacao via backend API com HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -9,6 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
     { provide: LOCALE_ID, useValue: 'pt' }
   ]
 };
