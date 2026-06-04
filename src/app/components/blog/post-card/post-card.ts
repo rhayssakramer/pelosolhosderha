da6fc297f5c8453d92b00a7c1426e8e84bcf159f
rhayssakramer <rhayssakramer@hotmail.com>
2026-06-04 16:50:12 -0300

    refactor: remover importação SafePipe não utilizada do componente PostCard

--- src/app/components/blog/post-card/post-card.ts
+++ src/app/components/blog/post-card/post-card.ts
@@ -2,11 +2,10 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Post } from '../../../models/post.model';
-import { SafePipe } from '../../../pipes/safe.pipe';
 
 @Component({
   selector: 'app-post-card',
-  imports: [CommonModule, RouterLink, SafePipe],
+  imports: [CommonModule, RouterLink],
   templateUrl: './post-card.html',
   styleUrl: './post-card.css'
 })
