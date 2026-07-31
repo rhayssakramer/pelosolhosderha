7363137c3f48eb35db09b71062fc1c29491a8537
rhayssakramer <rhayssakramer@hotmail.com>
2026-07-31 15:19:59 -0300

    refactor: alterar o ambiente de produção para true no arquivo environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://pelosolhosderha-api.bluesea-ecfbf889.brazilsouth.azurecontainerapps.io/api',
   siteUrl: 'https://pelosolhosderhastore.z20.web.core.windows.net'
 };
