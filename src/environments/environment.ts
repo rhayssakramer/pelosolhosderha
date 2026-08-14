225474ef027a9414dc7ba381003891d7e04ca63f
rhayssakramer <rhayssakramer@hotmail.com>
2026-08-14 14:36:21 -0300

    feat: atualizar o script de build para usar a configuração de produção e corrigir a URL do site

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://pelosolhosderha-api.bluesea-ecfbf889.brazilsouth.azurecontainerapps.io/api',
-  siteUrl: 'https://pelosolhosderhastore.z20.web.core.windows.net'
+  siteUrl: 'https://www.pelosolhosderha.com.br'
 };
